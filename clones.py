04e267d9989cfa9a2f8ddc3f932102be5f001ed0
j-x-mochiuti-x <joaomochi@gmail.com>
2026-01-28 16:51:19 -0300

    modelo teste para clones

---
+++ clones.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import tree
+
+df = pd.read_parquet("dados\dados_clones.parquet")
+
+# Separando variaveis e a reposta
+variaves = ["Massa(em kilos)", "Estatura(cm)"]
+x= df[variaves]
+target = 'Status '
+y= df[target]
+
+
+# Escolha do modelo
+modelo = tree.DecisionTreeClassifier()
+modelo.fit(X=x, y=y)
+
+
+# Arvore de decisão plotada
+plt.figure(figsize=(12,8))
+tree.plot_tree(modelo, feature_names=variaves, class_names=modelo.classes_, filled=True)
+plt.show()
+
+# %%
